d56e348487016c312430f8f7abf7f06a3f1cb40a
susantabiswas <susanta18@outlook.com>
2018-03-05 23:12:18 +0530

    updated module

--- utility.py
+++ utility.py
@@ -1,26 +1,5 @@
-from keras.models import Model
-from keras.layers import Input, Dense, Reshape, merge
-from keras.layers.embeddings import Embedding
-from keras.preprocessing.sequence import skipgrams
-from keras.preprocessing import sequence
+import numpy as np 
 
-import urllib.request
-import collections
-import os
-import zipfile
-
-import numpy as np
-import tensorflow as tf
-
-window_size = 3
-vector_dim = 300
-epochs = 1000
-
-valid_size = 16     # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
-valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-
-    
 # for loading the glove word embedding matrix values
 def load_glove_vectors(glove_file):
     with open(glove_file, 'r', encoding="utf-8") as file:
